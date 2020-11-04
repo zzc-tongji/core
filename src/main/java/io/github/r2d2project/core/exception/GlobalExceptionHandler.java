f4b2e1291363aaa38abb051c5af0db0abab8efb1
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2020-11-04 13:34:32 -0500

    Update.

--- src/main/java/io/github/r2d2project/core/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/r2d2project/core/exception/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
